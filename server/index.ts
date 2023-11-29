3fac35a55e5c5701d4196ebf9bb9c371b92933f5
suKyoung Shin <sukyoung.dev@gmail.com>
2023-11-29 16:59:35 +0900

    #1 최초 셋업

---
+++ server/index.ts
@@ -0,0 +1,39 @@
+const { createClient } = require("@supabase/supabase-js");
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+
+const supabaseUrl = "";
+const supabaseKey = "";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const typeDefs = `#graphql
+  type Query {}
+`;
+
+const resolvers = {
+  Query: {
+
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+
+(async () => {
+  try {
+    const { url } = await startStandaloneServer(server, {
+      listen: { port: 4000 },
+    });
+    console.log(`🚀  Server ready at: ${url}`);
+  } catch (e) {
+    console.log(e);
+  }
+  // `text` is not available here
+})();
